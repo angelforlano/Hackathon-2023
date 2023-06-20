66ab4836b71fa8e83a8ff3fa02f277584af9b12d
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-06-20 20:01:48 +0200

    main info done

--- FrontEnd/src/pages/home/Home.jsx
+++ FrontEnd/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import Footer from "../../components/footer/Footer"
 import NavbarLocal from "../../components/navbar/NavbarLocal"
 import Search from "../../components/search/Search";
+import MainInfo from '../../components/mainInfo/MainInfo';
+
 function Home() {
   return (
     <div>
       <NavbarLocal/>
+      <MainInfo/>
       <Search />
       <Footer/>
     </div>
