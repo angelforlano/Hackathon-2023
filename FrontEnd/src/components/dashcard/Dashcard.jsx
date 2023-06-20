0a51e97a8e3e5559df6ab715df14a1bfb3c2a2a4
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-06-20 19:01:36 +0200

    dashboard page without navbar and footer

--- FrontEnd/src/components/dashcard/Dashcard.jsx
+++ FrontEnd/src/components/dashcard/Dashcard.jsx
@@ -1,13 +1,21 @@
 import Iframe from "react-iframe";
 import './Dashcard.css';
+import { Button } from "react-bootstrap";
 
 function Dashcard() {
   return (
     <>
     <div className="cont-dashcard">
-      <p className="cont-dashcard-text">Mapa</p>
+      <p className="cont-dashcard-text">Resultats</p>
         <Iframe className="dashcard" src="https://charts.mongodb.com/charts-hackathon-2023-uxipt/embed/charts?id=6491bcb6-3e11-4ad8-888c-d9baa7beb917&maxDataAge=3600&theme=light&autoRefresh=true"/>
     </div> 
+    <div className="cont-indicators">
+      <p className="txt-indicator">Indicador 1</p>
+      <p className="txt-indicator">Indicador 2</p>
+      <p className="txt-indicator">Indicador 3</p>
+      <p className="txt-indicator">Indicador 4</p>
+    </div>
+    <Button className="butn-details">Veure més detalls</Button>
     </>
    
   )
