5d7aeba2bbb280c300536da6a67285f5da164a4d
Shima Naderi <shima.naderii@gmail.com>
2023-06-20 15:48:55 +0200

    add footer

--- FrontEnd/src/components/footer/Footer.jsx
+++ FrontEnd/src/components/footer/Footer.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>footer</div>
+    <div>
+       <ul>
+         <li>Blog</li>
+         <li>Nosaltres</li>
+         <li>Cookies</li>
+         <li>Privacitat</li>
+         <li>Termes i condicions</li>
+      </ul>
+      <div className="social-icons">
+         <FontAwesomeIcon icon={faFacebook} />
+         <FontAwesomeIcon icon={faInstagram} />
+         <FontAwesomeIcon icon={faPinterest} />
+      </div>
+
+      <p>Copyright © 2023 LocalShop. All Rights Reserved.</p>
+    </div>
   )
 }
 
