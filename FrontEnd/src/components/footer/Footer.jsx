b4e059fee69dfbe7b3390aa0de8676dffb395799
AleMCuitino <ale.morales11@gmail.com>
2023-06-20 23:05:30 +0200

    changes to visualice home page

--- FrontEnd/src/components/footer/Footer.jsx
+++ FrontEnd/src/components/footer/Footer.jsx
@@ -6,21 +6,23 @@ import Pinterest from '../../assets/images/Pinterest.svg';
 
 function Footer() {
   return (
-    <div className='footer-total'>
-       <ul className='footer-text'>
-         <li>Blog</li>
-         <li>Nosaltres</li>
-         <li>Cookies</li>
-         <li>Privacitat</li>
-         <li>Termes i condicions</li>
-      </ul>
-      <div className='social-icons'>
-         <img className='Inastagram-logo' src={Instagram} alt='instagram-icon'/>
-         <img className='FaceBook-logo' src={Facebook} alt='FaceBook-icon'/>
-         <img className='Pinterest-logo' src={Pinterest} alt='Pinterest-icon'/>
-      </div>
+    <div className='footer-container'>
+      <div className='footer-total'>
+        <ul className='footer-text'>
+          <li>Blog</li>
+          <li>Nosaltres</li>
+          <li>Cookies</li>
+          <li>Privacitat</li>
+          <li>Termes i condicions</li>
+        </ul>
+        <div className='social-icons'>
+          <img className='Inastagram-logo' src={Instagram} alt='instagram-icon'/>
+          <img className='FaceBook-logo' src={Facebook} alt='FaceBook-icon'/>
+          <img className='Pinterest-logo' src={Pinterest} alt='Pinterest-icon'/>
+        </div>
 
-      <p className='copy-right'>Copyright © 2023 LocalShop. All Rights Reserved.</p>
+        <p className='copy-right'>Copyright © 2023 LocalShop. All Rights Reserved.</p>
+      </div>
     </div>
   )
 }
