b51e23c41c9bef3de1926d708ce6544dab130a5a
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-06-20 19:04:29 +0200

    dashboard page with navbar

--- FrontEnd/src/pages/dashboard/Dashboard.jsx
+++ FrontEnd/src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,10 @@
 import Dashcard from '../../components/dashcard/Dashcard'
+import NavbarLocal from '../../components/navbar/NavbarLocal'
 
 function Dashboard() {
   return (
     <div>
+      <NavbarLocal/>
       <Dashcard/>
     </div>
   )
