e8bffbe7d12df8cdf2db0b44dfda134681206f65
AleMCuitino <ale.morales11@gmail.com>
2023-06-20 17:13:48 +0200

    Resolved conflict

--- FrontEnd/src/pages/home/Home.jsx
+++ FrontEnd/src/pages/home/Home.jsx
@@ -1,19 +1,11 @@
 import MainInfo from "../../components/mainInfo/MainInfo"
 import NavbarLocal from "../../components/navbar/NavbarLocal"
-<<<<<<< HEAD
-
-=======
 import Search from "../../components/search/Search";
->>>>>>> 2a732fc3a5c331ba4770eb6c46f9d14578b8d2de
 function Home() {
   return (
     <div>
       <NavbarLocal/>
-<<<<<<< HEAD
-      <MainInfo/>
-=======
       <Search />
->>>>>>> 2a732fc3a5c331ba4770eb6c46f9d14578b8d2de
     </div>
   )
 }
