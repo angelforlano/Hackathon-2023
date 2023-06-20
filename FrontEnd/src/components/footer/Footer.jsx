4b0a14537e8e598ea937ede94508d3b116937dae
Shima Naderi <shima.naderii@gmail.com>
2023-06-20 16:10:58 +0200

    add icons

--- FrontEnd/src/components/footer/Footer.jsx
+++ FrontEnd/src/components/footer/Footer.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Footer from 'react';
+import faInstagram from './assets/images/faInstagram.png';
+import faFacebook from './src/assets/images/faInstagram.png'; 
+import faPinterest from '../src/assets/images.faPinterest.png';
 
 function Footer() {
   return (
@@ -11,9 +15,9 @@ function Footer() {
          <li>Termes i condicions</li>
       </ul>
       <div className="social-icons">
-         <FontAwesomeIcon icon={faFacebook} />
-         <FontAwesomeIcon icon={faInstagram} />
-         <FontAwesomeIcon icon={faPinterest} />
+         <img icon={faFacebook} />
+         <img icon={faInstagram} />
+         <img icon={faPinterest} />
       </div>
 
       <p>Copyright © 2023 LocalShop. All Rights Reserved.</p>
