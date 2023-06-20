46160ebbace2f76dec6fd186943fb037c8065442
Angel <angel12345leo@gmail.com>
2023-06-20 12:38:01 +0200

    *

--- Test.py
+++ Test.py
@@ -4,8 +4,19 @@ file = "data/2019_censcomercialbcn_detall.csv"
 
 df = pandas.read_csv(file)
 
+print(list(df.columns))
 print(df.head())
 print(df.describe())
+
+
+def get_all_colls():
+    for col in df.columns:
+        if ("Codi_" in col):
+            print(col)
+            print(df[col].unique())
+
+print(df["Codi_Principal_Activitat"].describe())
+print(df["Codi_Principal_Activitat"].unique())
 
 """
 with open("data/2019_censcomercialbcn_detall.csv", "r", encoding="utf-8") as file:
